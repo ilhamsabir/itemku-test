714e9d9c9289c6b8cbba68f05111cdccb1f91cd7
Rico Sandyca <ricosandyca.if@gmail.com>
2021-01-08 13:15:12 +0700

    Question 1

---
+++ 01-chat.js
@@ -0,0 +1,113 @@
+/**
+ * Possible command
+ * Case sensitive disable
+ */
+const ENUM_COMMAND = {
+  ENTER: "enter",
+  LEAVE: "leave",
+  CHANGE: "change"
+}
+
+/**
+ * Simple switch helper
+ * 
+ * @param {any} value - value to compare
+ * @param {Object} statement - object of statement (key as val, value as callback)
+ * @param {Function|undefined} defaultCallback - default execution when none mathes
+ * @return {void}
+ */
+function simpleSwitch(value, statement, defaultCallback) {
+  statement[value]
+    ? statement[value]()
+    : defaultCallback && defaultCallback()
+}
+
+/**
+ * Main solution function
+ * 
+ * @param {String[]} records - chat input
+ * @returns {String[]}
+ */
+function solution(records) {
+  let answers = [] // output results
+  let users = [] // users in the chat (id, name)
+  let activities = [] // detail chat activities (id, command)
+
+  /**
+   * Get user detail by user id
+   * 
+   * @param {String} userId - user id to get
+   * @returns {Object} user data
+   */
+  const getUser = (userId) => {
+    return users.find(({ id }) => id === userId)
+  }
+
+  /**
+   * Insert new user to user list
+   * Replace if exists
+   * 
+   * @param {Object} newUser - user data to add
+   * @param {String} newUser.id - user id
+   * @param {String} newUser.name - user name
+   * @returns {void}
+   */
+  const insertUser = (newUser) => {
+    for (const i in users) {
+      // check does user id already exist?
+      if (users[i].id === newUser.id) {
+        users[i] = newUser
+        return
+      }
+    }
+    users.push(newUser)
+  }
+
+  /**
+   * Remove user by user id
+   * 
+   * @param {String} userId - user id to remove
+   * @returns {void}
+   */
+  const removeUser = (userId) => {
+    users.filter(({ id }) => id !== userId)
+  }
+
+  // build detail activities
+  records.forEach(record => {
+    // split string by space
+    const [command, userId, info] = record.split(" ")
+    activities.push({ userId, command })
+
+    // conditional process by command
+    // convert command string into lowercase to disable case sensitive
+    simpleSwitch(command.toLowerCase(), {
+      // insert new user on enter
+      [ENUM_COMMAND.ENTER]: () => insertUser({ id: userId, name: info }),
+      // remove user on enter
+      [ENUM_COMMAND.LEAVE]: () => removeUser(userId),
+      // insert or replace user on change
+      [ENUM_COMMAND.CHANGE]: () => insertUser({ id: userId, name: info })
+    })
+  })
+
+  // build readable chat message
+  activities.forEach(activity => {
+    // conditional process by command
+    // create output message
+    simpleSwitch(activity.command.toLowerCase(), {
+      [ENUM_COMMAND.ENTER]: () => answers.push(`${getUser(activity.userId).name} came in`),
+      [ENUM_COMMAND.LEAVE]: () => answers.push(`${getUser(activity.userId).name} has left`)
+    })
+  })
+
+  return answers
+}
+
+console.log(solution([
+  "Enter uid1234 Muzi",
+  "Enter uid4567 Prodo",
+  "Leave uid1234",
+  "Enter uid1234 Prodo",
+  "Change uid4567 Ryan"
+])) // [ 'Prodo came in', 'Ryan came in', 'Prodo has left', 'Prodo came in' ]
